2c6d21bb2f1f786944b82297fecfa25260cf78ba
AitijyaSarker <aitijyasarker@gmail.com>
2024-05-17 20:53:59 +0600

    Problem-2.c

--- Problem-2.c
+++ Problem-2.c
@@ -2,11 +2,11 @@
 #include <math.h>
 
 int main() {
-    // Defining the angle in degrees
+    
     double theta_degrees = 45.0;
-    // Converting angle to radians for calculation
+    
     double theta_radians = theta_degrees * (M_PI / 180.0);
-    // Calculating the ratio using the sine of the angle
+   
     double ratio = 1 / sin(theta_radians);
 
     printf("The ratio of the slit width to the wavelength for the first diffraction minimum at 45 degrees is: %f\n", ratio);
