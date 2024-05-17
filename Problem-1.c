75dee037d7a641d02f986b776be264fb9af85644
AitijyaSarker <aitijyasarker@gmail.com>
2024-05-17 20:53:18 +0600

    Problem-1.c

--- Problem-1.c
+++ Problem-1.c
@@ -9,7 +9,7 @@ int main() {
     printf("Enter the wavelength λ (in nm): ");
     scanf("%lf", &lambda);
     
-    // Checking if the wavelength is within the valid range
+    
     if(lambda < 380 || lambda > 750) {
         printf("Out of the range. Please enter a valid number.\n");
         return 1;
@@ -20,16 +20,16 @@ int main() {
     printf("Enter the slit separation d (in μm): ");
     scanf("%lf", &d);
     
-    // Converting angle from degrees to radians
+    
     theta = theta * (M_PI / 180.0);
     
-    // Converting d from μm to nm (1 μm = 1000 nm)
+   
     d = d * 1000;
     
-    // Calculating the order of the maxima
+    
     m = (int)(d * sin(theta) / lambda);
     
-    // Output of the result
+    
     printf("%d-th order maxima\n", m);
     
     return 0;
